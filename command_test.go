9ccf473b72740dea2c882099afc599ee1d5d61e3
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:16 -0800

    tests

--- command_test.go
+++ command_test.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	gloo "github.com/gloo-foo/framework"
 	"github.com/yupsh/echo"
-	yup "github.com/gloo-foo/framework"
-	. "github.com/gloo-foo/pipe"
 	"github.com/yupsh/xargs"
 )
 
 func ExampleXargs() {
 	// echo "one\ntwo\nthree" | xargs echo
-	if err := yup.Run(Pipeline(
+	if err := gloo.Run(Pipeline(
 		echo.Echo("one\ntwo\nthree"),
 		xargs.Xargs(echo.Echo()),
 	)); err != nil {
